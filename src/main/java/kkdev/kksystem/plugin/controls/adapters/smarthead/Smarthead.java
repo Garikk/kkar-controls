c14b78f070e23db588305f8e5b57fc980b9a70bb
Garikk <supergarikk@gmail.com>
2016-05-13 20:27:33 +0300

    Smarthead controls completed

--- src/main/java/kkdev/kksystem/plugin/controls/adapters/smarthead/Smarthead.java
+++ src/main/java/kkdev/kksystem/plugin/controls/adapters/smarthead/Smarthead.java
@@ -5,17 +5,13 @@
  */
 package kkdev.kksystem.plugin.controls.adapters.smarthead;
 
-import kkdev.kksystem.plugin.controls.adapters.debug.*;
 import java.util.HashMap;
 import java.util.Map;
 import kkdev.kksystem.base.classes.base.PinBaseDataTaggedObj;
-import static kkdev.kksystem.base.classes.controls.PinControlData.*;
-import kkdev.kksystem.base.classes.plugins.PluginMessage;
 import kkdev.kksystem.plugin.controls.adapters.IHWAdapter;
 import kkdev.kksystem.plugin.controls.adapters.IHWAdapterCallback;
 import kkdev.kksystem.plugin.controls.configuration.Adapter;
 import kkdev.kksystem.plugin.controls.configuration.Control;
-import kkdev.kksystem.plugin.controls.configuration.PluginSettings;
 
 /**
  *
@@ -25,7 +21,7 @@ public class Smarthead implements IHWAdapter {
 
     final String SmartheadControlPFX = "$K_CTRL_";
     final String SmartheadControlEvt_PRESS = "$KPR_";
-    final String SmartheadControlEvt_HOLD = "$KRL_";
+    final String SmartheadControlEvt_HOLD = "$KHL_";
     final String SmartheadControlEvt_RELEASE = "$KRL_";
     IHWAdapterCallback CB;
     Map<String, Control> Controls;
@@ -78,6 +74,12 @@ public class Smarthead implements IHWAdapter {
             SmartheadString = SmartheadString.substring(SmartheadControlEvt_PRESS.length());
             if (Controls.containsKey(SmartheadString)) {
                 CB.Control_Triggered(Controls.get(SmartheadString));
+            }
+        }
+        else if (SmartheadString.startsWith(SmartheadControlEvt_HOLD)) {
+            SmartheadString = SmartheadString.substring(SmartheadControlEvt_HOLD.length());
+            if (Controls.containsKey(SmartheadString)) {
+                CB.Control_LongPress(Controls.get(SmartheadString),1);
             }
         }
     }
