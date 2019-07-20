0394c378189d0e00e1ccdf00ee4da6da427adbf9
Garikk <supergarikk@gmail.com>
2019-07-20 21:55:10 +0300

    refactor up to jdk12

--- src/main/java/kkdev/kksystem/plugin/controls/configuration/kk_DefaultConfig.java
+++ src/main/java/kkdev/kksystem/plugin/controls/configuration/kk_DefaultConfig.java
@@ -9,68 +9,66 @@ import java.util.HashSet;
 import static kkdev.kksystem.base.classes.controls.PinDataControl.*;
 import kkdev.kksystem.plugin.controls.configuration.ControlsConfig.AdapterType;
 
-
 /**
  *
  * @author blinov_is
  *
-
+ *
  */
 public abstract class kk_DefaultConfig {
+
     public static ControlsConfig MakeDefaultConfig() {
-        
+
         ControlsConfig DefConf = new ControlsConfig();
-        
-        DefConf.Adapters=new Adapter[4];
-        
+
+        DefConf.Adapters = new Adapter[4];
+
         DefConf.Adapters[0] = new Adapter();
-        DefConf.Adapters[0].ID="KK_RPI_Adapter";
-        DefConf.Adapters[0].Name="Raspberry";
-        DefConf.Adapters[0].Type=AdapterType.RaspberryPI_B;
-        
+        DefConf.Adapters[0].ID = "KK_RPI_Adapter";
+        DefConf.Adapters[0].Name = "Raspberry";
+        DefConf.Adapters[0].Type = AdapterType.RaspberryPI_B;
+
         DefConf.Adapters[1] = new Adapter();
-        DefConf.Adapters[1].ID="DBG_Adapter";
-        DefConf.Adapters[1].Name="Debug";
-        DefConf.Adapters[1].Type=AdapterType.Debug;
+        DefConf.Adapters[1].ID = "DBG_Adapter";
+        DefConf.Adapters[1].Name = "Debug";
+        DefConf.Adapters[1].Type = AdapterType.Debug;
         //
         DefConf.Adapters[2] = new Adapter();
-        DefConf.Adapters[2].ID="RPI_I2C";
-        DefConf.Adapters[2].Name="I2C_Buttons";
-        DefConf.Adapters[2].Type=AdapterType.RaspberryPI_B_PI4J_I2C;
-        DefConf.Adapters[2].BusID=1;
-        DefConf.Adapters[2].DeviceID=0x2b;
+        DefConf.Adapters[2].ID = "RPI_I2C";
+        DefConf.Adapters[2].Name = "I2C_Buttons";
+        DefConf.Adapters[2].Type = AdapterType.RaspberryPI_B_PI4J_I2C;
+        DefConf.Adapters[2].BusID = 1;
+        DefConf.Adapters[2].DeviceID = 0x2b;
         //
-         //
-        DefConf.Adapters[3] = new Adapter();
-        DefConf.Adapters[3].ID="UNI_COM";
-        DefConf.Adapters[3].Name="COM_Buttons";
-        DefConf.Adapters[3].Type=AdapterType.UniversalLinux_RS232;
-        DefConf.Adapters[3].BusID=1;
-        DefConf.Adapters[3].DeviceID=0x2b;
         //
-               //
         DefConf.Adapters[3] = new Adapter();
-        DefConf.Adapters[3].ID="SMARTHEAD";
-        DefConf.Adapters[3].Name="SMARTHEAD_Buttons";
-        DefConf.Adapters[3].Type=AdapterType.KKSmarthead;
-        DefConf.Adapters[3].UNILPort="SMARTHEAD";
+        DefConf.Adapters[3].ID = "UNI_COM";
+        DefConf.Adapters[3].Name = "COM_Buttons";
+        DefConf.Adapters[3].Type = AdapterType.UniversalLinux_RS232;
+        DefConf.Adapters[3].BusID = 1;
+        DefConf.Adapters[3].DeviceID = 0x2b;
+        //
+        //
+        DefConf.Adapters[3] = new Adapter();
+        DefConf.Adapters[3].ID = "SMARTHEAD";
+        DefConf.Adapters[3].Name = "SMARTHEAD_Buttons";
+        DefConf.Adapters[3].Type = AdapterType.KKSmarthead;
+        DefConf.Adapters[3].UNILPort = "SMARTHEAD";
         //
         // 
-        DefConf.Controls=new Control[26];
+        DefConf.Controls = new Control[26];
         // RPI controls, for debug
         DefConf.Controls[0] = new Control();
         DefConf.Controls[1] = new Control();
         DefConf.Controls[2] = new Control();
         DefConf.Controls[3] = new Control();
-        
+
         // Custom chrysler head REMOVE THIS IN PRODUCTION
-        for (int i=4;i<DefConf.Controls.length;i++)
-        {
+        for (int i = 4; i < DefConf.Controls.length; i++) {
             DefConf.Controls[i] = new Control();
         }
-        for (int i=0;i<DefConf.Controls.length;i++)
-        {
-            DefConf.Controls[i].buttonID = new HashSet<>();
+        for (Control Control : DefConf.Controls) {
+            Control.buttonID = new HashSet<>();
         }
         //RPI, debug pri gpio buttons
         DefConf.Controls[0].Name = "SelectUP";
@@ -87,50 +85,47 @@ public abstract class kk_DefaultConfig {
         //
         DefConf.Controls[3].Name = "Back";
         DefConf.Controls[3].buttonID.add(DEF_BTN_BACK);
-        DefConf.Controls[3].AdapterSource="14";
-        
-        
-        // Custom chrysler head REMOVE THIS IN PRODUCTION
-        DefConf.Controls[4].Name="CUSTOM_CHRY_TRACK_1";
-        DefConf.Controls[4].buttonID.add("CUSTOM_CHRY_TRACK_1");
-        DefConf.Controls[4].AdapterSource="1";
+        DefConf.Controls[3].AdapterSource = "14";
 
-        DefConf.Controls[5].Name="CUSTOM_CHRY_TRACK_2";
+        // Custom chrysler head REMOVE THIS IN PRODUCTION
+        DefConf.Controls[4].Name = "CUSTOM_CHRY_TRACK_1";
+        DefConf.Controls[4].buttonID.add("CUSTOM_CHRY_TRACK_1");
+        DefConf.Controls[4].AdapterSource = "1";
+
+        DefConf.Controls[5].Name = "CUSTOM_CHRY_TRACK_2";
         DefConf.Controls[5].buttonID.add("CUSTOM_CHRY_TRACK_2");
-        DefConf.Controls[5].AdapterSource="2"; 
-        
-        DefConf.Controls[6].Name="CUSTOM_CHRY_TRACK_3";
+        DefConf.Controls[5].AdapterSource = "2";
+
+        DefConf.Controls[6].Name = "CUSTOM_CHRY_TRACK_3";
         DefConf.Controls[6].buttonID.add("CUSTOM_CHRY_TRACK_3");
-        DefConf.Controls[6].AdapterSource="3"; 
-       
-        DefConf.Controls[7].Name="CUSTOM_CHRY_TRACK_4";
+        DefConf.Controls[6].AdapterSource = "3";
+
+        DefConf.Controls[7].Name = "CUSTOM_CHRY_TRACK_4";
         DefConf.Controls[7].buttonID.add("CUSTOM_CHRY_TRACK_4");
-        DefConf.Controls[7].AdapterSource="4"; 
-        
-        DefConf.Controls[8].Name="CUSTOM_CHRY_TRACK_5";
+        DefConf.Controls[7].AdapterSource = "4";
+
+        DefConf.Controls[8].Name = "CUSTOM_CHRY_TRACK_5";
         DefConf.Controls[8].buttonID.add("CUSTOM_CHRY_TRACK_5");
-        DefConf.Controls[8].AdapterSource="5"; 
-        
-        DefConf.Controls[9].Name="CUSTOM_CHRY_SEEK_UP";
+        DefConf.Controls[8].AdapterSource = "5";
+
+        DefConf.Controls[9].Name = "CUSTOM_CHRY_SEEK_UP";
         DefConf.Controls[9].buttonID.add("CUSTOM_CHRY_SEEK_UP");
         DefConf.Controls[9].buttonID.add(DEF_BTN_UP);
         DefConf.Controls[9].buttonID.add(DEF_BTN_SEEK_FF);
-        DefConf.Controls[9].AdapterSource="U"; 
-        
-   
-        DefConf.Controls[10].Name="CUSTOM_CHRY_SEEK_DOWN";
+        DefConf.Controls[9].AdapterSource = "U";
+
+        DefConf.Controls[10].Name = "CUSTOM_CHRY_SEEK_DOWN";
         DefConf.Controls[10].buttonID.add("CUSTOM_CHRY_SEEK_DOWN");
         DefConf.Controls[10].buttonID.add(DEF_BTN_DOWN);
         DefConf.Controls[10].buttonID.add(DEF_BTN_SEEK_RW);
-        DefConf.Controls[10].AdapterSource="D"; 
-        
-        
-        DefConf.Controls[11].Name="CUSTOM_CHRY_AMFM_AMFM";
+        DefConf.Controls[10].AdapterSource = "D";
+
+        DefConf.Controls[11].Name = "CUSTOM_CHRY_AMFM_AMFM";
         DefConf.Controls[11].buttonID.add("CUSTOM_CHRY_AMFM_AMFM");
         DefConf.Controls[11].buttonID.add(DEF_BTN_BACK);
-        DefConf.Controls[11].AdapterSource="A"; 
-        
-        DefConf.Controls[12].Name="CUSTOM_CHRY_AMFM_SET";
+        DefConf.Controls[11].AdapterSource = "A";
+
+        DefConf.Controls[12].Name = "CUSTOM_CHRY_AMFM_SET";
         DefConf.Controls[12].buttonID.add("CUSTOM_CHRY_AMFM_SET");
         DefConf.Controls[12].buttonID.add(DEF_BTN_ENTER);
         DefConf.Controls[12].AdapterSource = "S";
@@ -139,18 +134,18 @@ public abstract class kk_DefaultConfig {
         DefConf.Controls[13].buttonID.add("CUSTOM_CHRY_CLOCK_H");
         DefConf.Controls[13].buttonID.add(DEF_BTN_VOL_INC);
         DefConf.Controls[13].AdapterSource = "H";
-        
+
         DefConf.Controls[14].Name = "CUSTOM_CHRY_CLOCK_M";
         DefConf.Controls[14].buttonID.add("CUSTOM_CHRY_CLOCK_M");
         DefConf.Controls[14].buttonID.add(DEF_BTN_VOL_DEC);
-        
+
         DefConf.Controls[14].AdapterSource = "M";
 
         DefConf.Controls[15].Name = "CUSTOM_CHRY_TUNE_FF";
         DefConf.Controls[15].buttonID.add("CUSTOM_CHRY_TUNE_FF");
         DefConf.Controls[15].buttonID.add(DEF_BTN_NEXT_TRACK);
         DefConf.Controls[15].AdapterSource = "F";
-        
+
         DefConf.Controls[16].Name = "CUSTOM_CHRY_TUNE_RW";
         DefConf.Controls[16].buttonID.add("CUSTOM_CHRY_TUNE_RW");
         DefConf.Controls[16].buttonID.add(DEF_BTN_PREV_TRACK);
@@ -164,24 +159,24 @@ public abstract class kk_DefaultConfig {
         DefConf.Controls[18].buttonID.add("CUSTOM_CHRY_VOL_UP");
         DefConf.Controls[18].buttonID.add(DEF_BTN_VOL_INC);
         DefConf.Controls[18].AdapterSource = "VOLDOWN"; //CHANGE!! wrong id in arduino code
-        
+
         DefConf.Controls[19].Name = "CUSTOM_CHRY_VOL_DOWN";
         DefConf.Controls[19].buttonID.add("CUSTOM_CHRY_VOL_DOWN");
         DefConf.Controls[19].buttonID.add(DEF_BTN_VOL_DEC);
         DefConf.Controls[19].AdapterSource = "VOLUP";//CHANGE!! wrong id in arduino code
-        
+
         DefConf.Controls[20].Name = "CUSTOM_CHRY_BAL_L";
         DefConf.Controls[20].buttonID.add("CUSTOM_CHRY_BAL_L");
         DefConf.Controls[20].AdapterSource = "BL";
-        
+
         DefConf.Controls[21].Name = "CUSTOM_CHRY_BAL_R";
         DefConf.Controls[21].buttonID.add("CUSTOM_CHRY_BAL_R");
         DefConf.Controls[21].AdapterSource = "BR";
-                
+
         DefConf.Controls[22].Name = "CUSTOM_CHRY_FADE_R";
         DefConf.Controls[22].buttonID.add("CUSTOM_CHRY_FADE_R");
         DefConf.Controls[22].AdapterSource = "FR";
-        
+
         DefConf.Controls[23].Name = "CUSTOM_CHRY_FADE_L";
         DefConf.Controls[23].buttonID.add("CUSTOM_CHRY_FADE_L");
         DefConf.Controls[23].AdapterSource = "FL";
@@ -189,36 +184,30 @@ public abstract class kk_DefaultConfig {
         DefConf.Controls[24].Name = "CUSTOM_CHRY_BASS";
         DefConf.Controls[24].buttonID.add("CUSTOM_CHRY_BASS");
         DefConf.Controls[24].AdapterSource = "BS";
-                
+
         DefConf.Controls[25].Name = "CUSTOM_CHRY_TREB";
         DefConf.Controls[25].buttonID.add("CUSTOM_CHRY_TREB");
         DefConf.Controls[25].AdapterSource = "TR";
-        
 
-        DefConf.Controls[0].AdapterID="DBG_Adapter";
-        DefConf.Controls[1].AdapterID="DBG_Adapter";
-        DefConf.Controls[2].AdapterID="DBG_Adapter";
-        DefConf.Controls[3].AdapterID="DBG_Adapter";
-        
-        for (int i=4;i<DefConf.Controls.length;i++)
-        {
-             DefConf.Controls[i].AdapterID="SMARTHEAD";
-        }
-        
+        DefConf.Controls[0].AdapterID = "DBG_Adapter";
+        DefConf.Controls[1].AdapterID = "DBG_Adapter";
+        DefConf.Controls[2].AdapterID = "DBG_Adapter";
+        DefConf.Controls[3].AdapterID = "DBG_Adapter";
 
-        
-        DefConf.Controls[0].Global=false;
-        DefConf.Controls[1].Global=false;
-        DefConf.Controls[2].Global=false;
-        DefConf.Controls[3].Global=true; //back button is global
-      
-        for (int i=4;i<DefConf.Controls.length;i++)
-        {
-            DefConf.Controls[i].Global=false;
+        for (int i = 4; i < DefConf.Controls.length; i++) {
+            DefConf.Controls[i].AdapterID = "SMARTHEAD";
         }
-         DefConf.Controls[11].Global=true;
-          DefConf.Controls[12].Global=true; //back button is global
+
+        DefConf.Controls[0].Global = false;
+        DefConf.Controls[1].Global = false;
+        DefConf.Controls[2].Global = false;
+        DefConf.Controls[3].Global = true; //back button is global
+
+        for (int i = 4; i < DefConf.Controls.length; i++) {
+            DefConf.Controls[i].Global = false;
+        }
+        DefConf.Controls[11].Global = true;
+        DefConf.Controls[12].Global = true; //back button is global
         return DefConf;
     }
 }
-    
