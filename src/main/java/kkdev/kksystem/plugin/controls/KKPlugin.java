fd84eb3f7d7902dcf8c2af099836ee8cff8ab7c7
Igor Blinov <i.blinov@um-agency.com>
2019-09-05 17:46:27 +0300

    ..

--- src/main/java/kkdev/kksystem/plugin/controls/KKPlugin.java
+++ src/main/java/kkdev/kksystem/plugin/controls/KKPlugin.java
@@ -1,7 +1,5 @@
 package kkdev.kksystem.plugin.controls;
 
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -21,34 +19,37 @@ import kkdev.kksystem.base.interfaces.IBaseConnection;
  * @author blinov_is
  */
 public final class KKPlugin extends KKPluginBase {
+
     public KKPlugin() {
         super(new ControlsPluginInfo());
-        Global.PM=new ControlsManager();
+        Global.PM = new ControlsManager();
     }
 
     @Override
     public void pluginInit(IBaseConnection BaseConnector, String GlobalConfUID) {
         super.pluginInit(BaseConnector, GlobalConfUID); //To change body of generated methods, choose Tools | Templates.
-         Global.PM.InitControls(this);
+        Global.PM.InitControls(this);
     }
 
-   
     @Override
     public void executePin(PluginMessage Pin) {
         super.executePin(Pin);
-       Global.PM.ReceivePin(Pin.pinName, (PinData) Pin.getPinData());
-        return ;
+        Global.PM.ReceivePin(Pin.pinName, (PinData) Pin.getPinData());
+        return;
     }
-     @Override
+
+    @Override
     public void pluginStart() {
-      Global.PM.PluginStart();
+        Global.PM.PluginStart();
     }
-     @Override
+
+    @Override
     public void pluginStop() {
-      Global.PM.PluginStop();
+        Global.PM.PluginStop();
     }
-     @Override
+
+    @Override
     public PluginConfiguration getPluginSettings() {
-       return PluginSettings.MainConfiguration;
+        return PluginSettings.MainConfiguration;
     }
 }
